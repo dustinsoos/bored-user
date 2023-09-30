d8f4726357eb8b5acd79f2296ecf22e7340a6b4f
Dustin Soos <dustinmsoos@gmail.com>
2023-09-30 13:18:16 -0400

    fixed avatar bug

--- app/exampleuser/page.tsx
+++ app/exampleuser/page.tsx
@@ -14,12 +14,10 @@ type userData = {
         first: string;
         last: string;
       };
+      picture: {
+        thumbnail: string;
+      };
     },
-    // {
-    //   picture: {
-    //     thumbnail: string;
-    //   };
-    // },
   ];
 };
 
@@ -45,25 +43,27 @@ async function getUser(): Promise<userData> {
 export default async function Example() {
   const [bored, user] = await Promise.all([getBored(), getUser()]);
   const { first, last } = user.results[0].name;
-  // const thumbnailUrl = user.results[0].picture.thumbnail;
+  const thumbnailUrl = user.results[0].picture.thumbnail;
 
   return (
-    <main className=" text-white text-center  mt-32 ">
-      <h1 className="text-5xl bg-gradient-to-b from-white  to-blue-600 text-transparent bg-clip-text">
+    <main className=" text-white text-center  mt-64 ">
+      <h1 className="text-6xl md:text-8xl xl:text-7xl font-medium bg-gradient-to-b from-white  to-blue-600 text-transparent bg-clip-text">
         Try Example
       </h1>
-      <section className="grid gap-2 mt-4">
-        <h3>Activity: {bored.activity}</h3>
-        <p>Type: {bored.type}</p>
-        <p>Participants Needed: {bored.participants}</p>
-        <p>
-          User: {first} {last}
-        </p>
-        {/* <img
+      <section className=" mt-4">
+        <div className="grid gap-2 lg:text-lg">
+          <h3>Activity: {bored.activity}</h3>
+          <p>Type: {bored.type}</p>
+          <p>Participants Needed: {bored.participants}</p>
+          <p>
+            User: {first} {last}
+          </p>
+        </div>
+        <img
           src={thumbnailUrl}
           alt="thumbnail-img"
-          className="mx-auto w-12 rounded-full"
-        /> */}
+          className="mx-auto w-12 rounded-full mt-2"
+        />
         <RefreshButton />
       </section>
     </main>
