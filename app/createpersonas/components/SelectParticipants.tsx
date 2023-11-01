5963c02b35b4a7ae390afba9a44af984c92a9e8b
Dustin Soos <dustinmsoos@gmail.com>
2023-11-01 12:06:14 -0400

    passing api data down to child/onclick button functionality

--- app/createpersonas/components/SelectParticipants.tsx
+++ app/createpersonas/components/SelectParticipants.tsx
@@ -1,77 +1,73 @@
-type boredData = {
-  participants: number;
+import { useEffect } from "react";
+
+interface Activity {
   activity: string;
-};
-async function getBored1(): Promise<boredData> {
-  const response = await fetch(
-    "http://www.boredapi.com/api/activity?participants=1",
-    {
-      cache: "no-store",
-    }
-  );
-
-  return response.json();
 }
 
-async function getBored2(): Promise<boredData> {
-  const response = await fetch(
-    "http://www.boredapi.com/api/activity?participants=2",
-    {
-      cache: "no-store",
-    }
-  );
-
-  return response.json();
+interface SelectParticipantsProps {
+  participants: number;
+  apiData: Activity | null;
+  setApiData: React.Dispatch<React.SetStateAction<Activity | null>>;
+  onParticipantChange: (numParticipants: number) => void;
 }
 
-async function getBored3(): Promise<boredData> {
-  const response = await fetch(
-    "http://www.boredapi.com/api/activity?participants=3",
-    {
-      cache: "no-store",
+export default function SelectParticipants({
+  participants,
+  apiData,
+  onParticipantChange,
+  setApiData,
+}: SelectParticipantsProps) {
+  useEffect(() => {
+    if (participants > 0) {
+      fetch(
+        `https://www.boredapi.com/api/activity?participants=${participants}`
+      )
+        .then((response) => response.json())
+        .then((data: Activity) => {
+          setApiData(data);
+        })
+        .catch((error) => {
+          console.error("Error fetching data: ", error);
+        });
     }
-  );
-
-  return response.json();
-}
-
-async function getBored4(): Promise<boredData> {
-  const response = await fetch(
-    "http://www.boredapi.com/api/activity?participants=4",
-    {
-      cache: "no-store",
-    }
-  );
-
-  return response.json();
-}
-
-export default async function SelectParticipants({}) {
-  const [bored1, bored2, bored3, bored4] = await Promise.all([
-    getBored1(),
-    getBored2(),
-    getBored3(),
-    getBored4(),
-  ]);
+  }, [participants, setApiData]);
   return (
     <main className="text-center">
       <h3 className=" text-2xl bg-gradient-to-b from-white  to-blue-600 text-transparent bg-clip-text">
         Pick Number of Participants
       </h3>
       <div className="flex gap-4 justify-center">
-        <button className=" bg-blue-900 px-4 py-2 hover:text-blue-900 hover:outline hover:outline-blue-900 hover:bg-slate-200">
+        <button
+          onClick={() => onParticipantChange(1)}
+          className=" bg-blue-900 px-4 py-2 hover:text-blue-900 hover:outline hover:outline-blue-900 hover:bg-slate-200"
+        >
           1
         </button>
-        <button className=" bg-blue-900 px-4 py-2 hover:text-blue-900 hover:outline hover:outline-blue-900 hover:bg-slate-200">
+        <button
+          onClick={() => onParticipantChange(2)}
+          className=" bg-blue-900 px-4 py-2 hover:text-blue-900 hover:outline hover:outline-blue-900 hover:bg-slate-200"
+        >
           2
         </button>
-        <button className=" bg-blue-900 px-4 py-2 hover:text-blue-900 hover:outline hover:outline-blue-900 hover:bg-slate-200">
+        <button
+          onClick={() => onParticipantChange(3)}
+          className=" bg-blue-900 px-4 py-2 hover:text-blue-900 hover:outline hover:outline-blue-900 hover:bg-slate-200"
+        >
           3
         </button>
-        <button className=" bg-blue-900 px-4 py-2 hover:text-blue-900 hover:outline hover:outline-blue-900 hover:bg-slate-200">
+        <button
+          onClick={() => onParticipantChange(4)}
+          className=" bg-blue-900 px-4 py-2 hover:text-blue-900 hover:outline hover:outline-blue-900 hover:bg-slate-200"
+        >
           4
         </button>
       </div>
+
+      {apiData && (
+        <p>
+          Activity for {participants}: {apiData.activity}
+        </p>
+      )}
     </main>
   );
 }
--- app/createpersonas/page.tsx
+++ app/createpersonas/page.tsx
@@ -4,8 +4,14 @@ import { useState } from "react";
 import CreatePerson from "./components/CreatePerson";
 import SelectParticipants from "./components/SelectParticipants";
 
+interface Activity {
+  activity: string;
+}
+
 const CreatePersonas = () => {
-  const [isClicked, setIsClicked] = useState(false);
+  const [participants, setParticipants] = useState<number>(0);
+  const [apiData, setApiData] = useState<Activity | null>(null);
+
   return (
     <main className="px-2 mt-32">
       <h1 className=" text-center text-5xl bg-gradient-to-b from-white  to-blue-600 text-transparent bg-clip-text mb-16">
@@ -13,7 +19,12 @@ const CreatePersonas = () => {
       </h1>
       <div className="grid gap-10 md:flex   text-white w-2/3 lg:w-1/2 mx-auto lg:justify-between">
         <CreatePerson />
-        <SelectParticipants />
+        <SelectParticipants
+          participants={participants}
+          apiData={apiData}
+          onParticipantChange={setParticipants}
+          setApiData={setApiData}
+        />
       </div>
     </main>
   );
